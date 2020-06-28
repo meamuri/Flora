478aba94e17475774ae8ccd2cd661f92c7f2615d
meamruri <meammf@yandex.ru>
2020-06-28 21:14:27 +0300

    design login page

--- Flora/ContentView.swift
+++ Flora/ContentView.swift
@@ -8,14 +8,34 @@
 
 import SwiftUI
 
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            WelcomeLabel()
+            Image("flora_app_welcome")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150)
+                .clipped()
+                .cornerRadius(150)
+                .padding(.bottom, 75)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WelcomeLabel: View {
+    var body: some View {
+        Text("Flora application login")
+            .fontWeight(.semibold)
+            .font(.largeTitle)
+            .padding(.bottom, 20)
     }
 }
