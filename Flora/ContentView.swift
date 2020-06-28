13bdd3d93efe31f7c38433f6b5a795913321c77a
meamruri <meammf@yandex.ru>
2020-06-28 22:38:55 +0300

    design login page

--- Flora/ContentView.swift
+++ Flora/ContentView.swift
@@ -11,17 +11,29 @@ import SwiftUI
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
     var body: some View {
         VStack {
             WelcomeLabel()
-            Image("flora_app_welcome")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 150, height: 150)
-                .clipped()
-                .cornerRadius(150)
-                .padding(.bottom, 75)
+            UserImage()
+            TextField("Username", text: $username)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20.0)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20.0)
+            Button(action: {
+                
+            }) {
+                LoginButtonContent()
+            }
         }
+        .padding()
     }
 }
 
@@ -33,9 +45,33 @@ struct ContentView_Previews: PreviewProvider {
 
 struct WelcomeLabel: View {
     var body: some View {
-        Text("Flora application login")
+        Text("Flora кадастр")
             .fontWeight(.semibold)
             .font(.largeTitle)
             .padding(.bottom, 20)
+    }
+}
+
+struct UserImage: View {
+    var body: some View {
+        Image("flora_app_welcome")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
+    }
+}
+
+struct LoginButtonContent: View {
+    var body: some View {
+        Text("Войти")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.green)
+            .cornerRadius(15.0)
     }
 }
