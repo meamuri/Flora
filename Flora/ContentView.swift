f27f776dc56c3199de57759330d75b9313aa1da7
meamruri <meammf@yandex.ru>
2020-06-28 21:05:18 +0300

    Initial Commit

---
+++ Flora/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flora
+//
+//  Created by Rom Dr on 28.06.2020.
+//  Copyright © 2020 Roman Dronov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
